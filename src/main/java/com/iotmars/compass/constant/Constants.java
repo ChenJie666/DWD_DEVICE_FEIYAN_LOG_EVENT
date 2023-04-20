dc47abf282a01205d7f6394f680df45dffa7ba7f
cj <792965772@qq.com>
2023-04-20 18:23:04 +0800

    修改checkpoint路径

--- src/main/java/com/iotmars/compass/constant/Constants.java
+++ src/main/java/com/iotmars/compass/constant/Constants.java
@@ -6,13 +6,13 @@ package com.iotmars.compass.constant;
  */
 public class Constants {
 
-    public static String SOURCE_KAFKA_GROUP_ID = "flink-device_change_log_app_test3";
+    public static String SOURCE_KAFKA_GROUP_ID = "flink-device_change_log_app_test3"; // 后序需要更换group_id
     public static String SOURCE_KAFKA_BOOTSTRAP_SERVERS = "192.168.101.179:9092,192.168.101.180:9092,192.168.101.181:9092";
     public static String SINK_KAFKA_BOOTSTRAP_SERVERS = "192.168.101.193:9092,192.168.101.194:9092,192.168.101.195:9092";
     public static String SINK_KAFKA_TOPIC = "dwd_device_feiyan_log_late_event";
     public static String SINK_KAFKA_LATE_TOPIC = "dwd_device_feiyan_log_late_event";
 
 //    public static String CHECKPOINT_STORAGE = "hdfs://192.168.101.193:8020/flink/checkpoint/msas_prod/msas_device_exceptions";
-    public static String CHECKPOINT_STORAGE = "hdfs:///flink/checkpoint/dwd_device_feiyan_log_event/prod";
+    public static String CHECKPOINT_STORAGE = "hdfs:///flink/checkpoint/device/dwd_device_feiyan_log_event/prod";
 //    public static int PARALLELISM = 1;
 }
